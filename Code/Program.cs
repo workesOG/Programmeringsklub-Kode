cb6e4353fd4b85340036f704ede414b9c6a02c87
workes <Bassdard2012@gmail.com>
2023-11-23 17:03:52 +0100

    Initial Commit

---
+++ Code/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Variables variables = new Variables();
+        Console.WriteLine(variables.x * variables.y);
+        Console.ReadLine();
+    }
+}
---
+++ Code/Scripts/Variables.cs
@@ -0,0 +1,6 @@
+namespace Code;
+class Variables
+{
+    public int x = 2;
+    public int y = 3;
+}
